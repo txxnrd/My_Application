1be16a7dab078c2cd5598bb0abc93296bcec6471
kimseokyung <seokyung1114@gmail.com>
2023-06-30 00:08:42 +0900

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	app/src/main/res/layout/activity_main.xml

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -20,7 +20,6 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), MainActivity2.class);
                 startActivity(intent);
-                //new!!
             }
         });
     }
