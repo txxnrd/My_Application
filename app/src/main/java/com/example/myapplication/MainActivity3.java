3c3af04338746c5d2fe6f620f99dc52a7a40522a
taeyunroh <nrbsld@korea.ac.kr>
2023-06-30 20:30:04 +0900

    오늘도 화이팅~!~!

---
+++ app/src/main/java/com/example/myapplication/MainActivity3.java
@@ -0,0 +1,17 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+    }
+}
