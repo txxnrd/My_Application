edc53c95b9376b4f494e7076011f43156da4c031
taeyunroh <nrbsld@korea.ac.kr>
2023-07-01 17:11:55 +0900

    오늘도 내일도 화이팅~!~!

---
+++ app/src/main/java/com/example/myapplication/MyRecyclerAdapter.java
@@ -0,0 +1,76 @@
+package com.example.myapplication;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import com.bumptech.glide.Glide;
+
+
+public class MyRecyclerAdapter extends RecyclerView.Adapter<MyRecyclerAdapter.ViewHolder> {
+
+    private ArrayList<FriendItem> mFriendList;
+    private ItemClickListener mClickListener;
+
+    @NonNull
+    @Override
+    public MyRecyclerAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_recyclerview, parent, false);
+        return new ViewHolder(view);
+    }
+    @Override
+    public void onBindViewHolder(@NonNull MyRecyclerAdapter.ViewHolder holder, int position) {
+        holder.onBind(mFriendList.get(position));
+    }
+    public void setFriendList(ArrayList<FriendItem> list) {
+        this.mFriendList = list;
+        notifyDataSetChanged();
+    }
+    public void setClickListener(ItemClickListener itemClickListener) {
+        this.mClickListener = itemClickListener;
+    }
+    public interface ItemClickListener {
+        void onItemClick(View view, int position);
+    }
+    @Override
+    public int getItemCount() {
+        return mFriendList.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        ImageView profile;
+        TextView name;
+        TextView message;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            profile = itemView.findViewById(R.id.profile);
+            name = itemView.findViewById(R.id.name);
+            message = itemView.findViewById(R.id.message);
+
+            itemView.setOnClickListener(this);
+        }
+
+        void onBind(FriendItem item) {
+            Glide.with(profile.getContext())
+                    .load(item.getImageUrl())
+                    .into(profile);
+            name.setText(item.getName());
+            // !@!@!@ 사진 옆에 표시할 내용 추가
+            // 힘내라 힘
+            message.setText(item.getNumber());
+        }
+
+        @Override
+        public void onClick(View view) {
+            if (mClickListener != null) mClickListener.onItemClick(view, getAdapterPosition());
+        }
+    }
+}
