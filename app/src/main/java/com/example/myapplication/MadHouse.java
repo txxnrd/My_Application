f699b66c7c46721fc7fc223400f6875ab40f1508
txxnrd <nrbsld@korea.ac.kr>
2023-07-04 00:58:39 +0900

    My commit message

--- app/src/main/java/com/example/myapplication/MadHouse.java
+++ app/src/main/java/com/example/myapplication/MadHouse.java
@@ -10,6 +10,8 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.myapplication.databinding.Fragment3Binding;
+
 import java.util.Locale;
 import java.util.concurrent.TimeUnit;
 
@@ -19,25 +21,44 @@ public class MadHouse extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_madhouse);
 
+        Button button = (Button) findViewById(R.id.button2); // Replace with your actual Button ID
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MadHouse.this, MadHouse_2.class);
+                startActivity(intent);
+            }
+        });
+
+
         // Load the last elapsed time from SharedPreferences
         SharedPreferences sharedPreferences = getSharedPreferences("myPreferences", Context.MODE_PRIVATE);
-        long lastElapsedTime = sharedPreferences.getLong("lastElapsedTime", 0)/1000;
-        long second = lastElapsedTime % 60;
-        long minute = (lastElapsedTime / 60) % 60;
-        long hour = lastElapsedTime / 3600;
+        long lastElapsedTime = (sharedPreferences.getLong("lastElapsedTime", 0))*-1; // Don't divide by 1000 here
+
+// Convert milliseconds to hours, minutes, and seconds
+        long hours = TimeUnit.MILLISECONDS.toHours(lastElapsedTime);
+        long minutes = TimeUnit.MILLISECONDS.toMinutes(lastElapsedTime) -
+                TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(lastElapsedTime));
+        long seconds = TimeUnit.MILLISECONDS.toSeconds(lastElapsedTime) -
+                TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(lastElapsedTime));
+
+// Now the time should be positive
+        String formattedTime = String.format(Locale.getDefault(), "%02d:%02d:%02d", hours, minutes, seconds);
+        TextView textView = findViewById(R.id.time_history); // Replace "your_textview_id" with your actual TextView ID
+        textView.setText(formattedTime);
+
 
         // Load the week from SharedPreferences
         SharedPreferences sharedPreferences_1 = getSharedPreferences("myWeek", Context.MODE_PRIVATE);
         long week = sharedPreferences_1.getLong("week", 1);
 
-        // Display the last elapsed time
-        TextView textView = findViewById(R.id.time_history);
 
-        String formattedTime = String.format(Locale.getDefault(), "%02d:%02d:%02d", hour, minute, second);
-        textView.setText(formattedTime);
+
+
 
         TextView textView_1 = findViewById(R.id.dateTextView); // Replace with your actual TextView ID
         textView_1.setText(String.valueOf(week)+"주차 기록");
     }
+
 }
 
